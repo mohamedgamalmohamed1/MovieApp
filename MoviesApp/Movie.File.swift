5a6f6a0b0b02cea5e9a61325495a7b140d5154b9
mohamed gamal <mohamedgamalmohamed1@icloud.com>
2019-07-06 14:55:54 +0200

    Movie App

---
+++ MoviesApp/Movie.File.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.File.swift
+//  MoviesApp
+//
+//  Created by mohamed gamal mohamed on 5/31/19.
+//  Copyright © 2019 mohamed gamal mohamed. All rights reserved.
+//
+
+import Foundation
---
+++ MoviesApp/MovieDetailsCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieDetailsCell.swift
+//  MoviesApp
+//
+//  Created by mohamed gamal mohamed on 5/31/19.
+//  Copyright © 2019 mohamed gamal mohamed. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
